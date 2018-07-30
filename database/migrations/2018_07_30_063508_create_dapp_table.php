a72e5a3cef0881b204644c96be92798aa7fbca24
ety001 <ety001@domyself.me>
2018-07-30 16:02:56 +0800

    dapp table created

---
+++ database/migrations/2018_07_30_063508_create_dapp_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDappTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dapp', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('app_name')->unique();
+            $table->text('description')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('secret_key', 32);
+            $table->string('callback_url')->nullable();
+            $table->string('withdraw_addr')->nullable();
+            $table->enum('status', ['on', 'off', 'waiting'])->default('on');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('dapp', function (Blueprint $table) {
+            $table->dropForeign('dapp_user_id_foreign');
+        });
+        Schema::dropIfExists('dapp');
+    }
+}
