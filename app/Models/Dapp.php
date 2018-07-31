d9e866d0f09ac5fc8e2f9ca8ce0361a03f7b4050
ety001 <ety001@domyself.me>
2018-07-31 20:40:51 +0800

    config models

--- app/Models/Dapp.php
+++ app/Models/Dapp.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Dapp extends Model
 {
-    //
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
+    protected $table = 'dapp';
 }
